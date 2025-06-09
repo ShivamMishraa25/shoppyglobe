330737400f7479a60879795bc79aa37ca538f4e4
ShivamMishraa25 <shivam.m4464@gmail.com>
2025-06-09 23:23:38 +0530

    first commit, starting project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import ProductList from './components/ProductList'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <ProductList />
+        </>
+    )
+}
+
+export default App
